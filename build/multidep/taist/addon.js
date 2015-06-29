423ab689fb8e3a5c5903c9d2e3a33cb15eb69df9
Vitaliy Makeev <w.makeev@gmail.com>
2015-06-29 23:49:08 +0500

    update multiversion-repository@2.0.3

--- build/multidep/taist/addon.js
+++ build/multidep/taist/addon.js
@@ -52,7 +52,7 @@ function init() {
 	var wrapDefine = __webpack_require__(7);
 
 	var requirejsCdn    = 'https://cdn.jsdelivr.net/requirejs/2.1.14/require.min.js';
-	var repositoryCdn   = 'https://cdn.rawgit.com/wmakeev/multiversion-repository/2.0.3/multiversion-repository.js';
+	var repositoryCdn   = 'https://cdn.rawgit.com/wmakeev/multiversion-repository/2.0.4/multiversion-repository.js';
 	// TODO move to cdn
 	var multiverCdn = 'https://rawgit.com/wmakeev/requirejs-multiversion/master/build/multiver';
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var resolver = require('./module-resolver');
 var wrapDefine = require('./wrap-define');
 
 var requirejsCdn    = 'https://cdn.jsdelivr.net/requirejs/2.1.14/require.min.js';
-var repositoryCdn   = 'https://cdn.rawgit.com/wmakeev/multiversion-repository/2.0.3/multiversion-repository.js';
+var repositoryCdn   = 'https://cdn.rawgit.com/wmakeev/multiversion-repository/2.0.4/multiversion-repository.js';
 // TODO move to cdn
 var multiverCdn = 'https://rawgit.com/wmakeev/requirejs-multiversion/master/build/multiver';
 
